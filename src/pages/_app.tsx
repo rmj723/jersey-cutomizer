3676153128d4526c7a0fc207931a2b1551b48e3e
flaaudwls <74338977+flaaudwls@users.noreply.github.com>
2023-07-03 13:58:20 -0400

    cc

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import { useRef } from 'react';
 import dynamic from 'next/dynamic';
 import Header from '@/config';
 import Layout from '@/components/dom/Layout';
-import { TransitionProvider, TransitionLayout } from '@/utils/transition';
 import '@/styles/index.scss';
 
 const Scene = dynamic(() => import('@/components/canvas/Scene'), { ssr: true });
@@ -12,32 +11,28 @@ export default function App({ Component, pageProps = { title: 'index' } }) {
 
   return (
     <>
-      <TransitionProvider>
-        <TransitionLayout>
-          <Header title={pageProps.title} />
-          <Layout ref={ref}>
-            {/* The canvas can either be in front of the dom or behind. If it is in front it can overlay contents.
-             * Setting the event source to a shared parent allows both the dom and the canvas to receive events.
-             * Since the event source is now shared, the canvas would block events, we prevent that with pointerEvents: none. */}
-            {Component?.canvas && (
-              <Scene
-                // @ts-ignore
-                className='pointer-events-none'
-                eventSource={ref}
-                eventPrefix='client'
-                shadows
-                camera={{
-                  fov: 50,
-                  position: [0, 0, 10],
-                  rotation: [-0.1, 0, 0],
-                }}>
-                {Component.canvas(pageProps)}
-              </Scene>
-            )}
-            <Component {...pageProps} />
-          </Layout>
-        </TransitionLayout>
-      </TransitionProvider>
+      <Header title={pageProps.title} />
+      <Layout ref={ref}>
+        {/* The canvas can either be in front of the dom or behind. If it is in front it can overlay contents.
+         * Setting the event source to a shared parent allows both the dom and the canvas to receive events.
+         * Since the event source is now shared, the canvas would block events, we prevent that with pointerEvents: none. */}
+        {Component?.canvas && (
+          <Scene
+            // @ts-ignore
+            className='pointer-events-none'
+            eventSource={ref}
+            eventPrefix='client'
+            shadows
+            camera={{
+              fov: 50,
+              position: [0, 0, 10],
+              rotation: [-0.1, 0, 0],
+            }}>
+            {Component.canvas(pageProps)}
+          </Scene>
+        )}
+        <Component {...pageProps} />
+      </Layout>
     </>
   );
 }
