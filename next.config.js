a3d6e3b4c0288813074ca7884710cc04706bc4d5
flaaudwls <74338977+flaaudwls@users.noreply.github.com>
2023-07-08 08:42:57 -0400

    add recording

--- next.config.js
+++ next.config.js
@@ -1,11 +1,29 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
-})
+});
 const nextConfig = {
   // uncomment the following snippet if using styled components
   // compiler: {
   //   styledComponents: true,
   // },
+  async headers() {
+    return [
+      {
+        source: '/',
+        headers: [
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'require-corp',
+          },
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin',
+          },
+        ],
+      },
+    ];
+  },
+
   experimental: {},
   images: {
     remotePatterns: [
@@ -42,43 +60,51 @@ const nextConfig = {
           },
         },
       ],
-    })
+    });
 
     // shader support
     config.module.rules.push({
       test: /\.(glsl|vs|fs|vert|frag)$/,
       exclude: /node_modules/,
-      use: [ 'raw-loader', 'glslify-loader' ],
-    })
+      use: ['raw-loader', 'glslify-loader'],
+    });
 
-    return config
+    return config;
   },
-}
+};
 
 // manage i18n
 if (process.env.EXPORT !== 'true') {
   nextConfig.i18n = {
-    locales: [ 'en', 'jp' ],
+    locales: ['en', 'jp'],
     defaultLocale: 'en',
-  }
+  };
 }
 
-const KEYS_TO_OMIT = [ 'webpackDevMiddleware', 'configOrigin', 'target', 'analyticsId', 'webpack5', 'amp', 'assetPrefix' ]
+const KEYS_TO_OMIT = [
+  'webpackDevMiddleware',
+  'configOrigin',
+  'target',
+  'analyticsId',
+  'webpack5',
+  'amp',
+  'assetPrefix',
+];
 
 module.exports = (_phase, { defaultConfig }) => {
-  const plugins = [ [ withBundleAnalyzer, {} ] ]
+  const plugins = [[withBundleAnalyzer, {}]];
 
-  const wConfig = plugins.reduce((acc, [ plugin, config ]) => plugin({ ...acc, ...config }), {
+  const wConfig = plugins.reduce((acc, [plugin, config]) => plugin({ ...acc, ...config }), {
     ...defaultConfig,
     ...nextConfig,
-  })
+  });
 
-  const finalConfig = {}
+  const finalConfig = {};
   Object.keys(wConfig).forEach((key) => {
     if (!KEYS_TO_OMIT.includes(key)) {
-      finalConfig[ key ] = wConfig[ key ]
+      finalConfig[key] = wConfig[key];
     }
-  })
+  });
 
-  return finalConfig
-}
+  return finalConfig;
+};
